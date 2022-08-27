52174df956a5df6a7630d0c99a2c5697e8e00b98
Tristan Cacqueray <tdecacqu@redhat.com>
2022-08-27 23:48:45 +0000

    Add Logger and Clock module

---
+++ src/Butler/History.hs
@@ -0,0 +1,40 @@
+module Butler.History (
+    History,
+    newHistory,
+    addHistory,
+    recentHistory,
+    oldestHistory,
+) where
+
+import Data.Sequence (Seq, (|>))
+import Data.Sequence qualified as Seq
+
+import Butler.Prelude
+
+data History a = History
+    { maxSize :: Natural
+    , history :: TVar (Seq a)
+    }
+
+newHistory :: Natural -> STM (History a)
+newHistory size = History size <$> newTVar mempty
+
+addHistory :: History a -> a -> STM ()
+addHistory h a =
+    modifyTVar' h.history $ \buffer ->
+        let cleanBuffer
+                | seqLength buffer > h.maxSize = Seq.drop 1 buffer
+                | otherwise = buffer
+         in (cleanBuffer |> a)
+
+recentHistory :: History a -> STM [a]
+recentHistory h = toList . Seq.reverse <$> readHistory h
+
+oldestHistory :: History a -> STM [a]
+oldestHistory h = toList <$> readHistory h
+
+readHistory :: History a -> STM (Seq a)
+readHistory h = readTVar h.history
+
+seqLength :: Seq a -> Natural
+seqLength = unsafeFrom . Seq.length
---
+++ test/ClockTest.hs
@@ -0,0 +1,14 @@
+module ClockTest where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Butler.Clock
+import Butler.Prelude
+
+test_time :: TestTree
+test_time = testCase "Time increase" do
+    clock <- newClock
+    sleep 10
+    Milli t <- from <$> clock.getTime
+    assertBool ("Elapsed time is out range: " <> show t) (t >= 10 && t < 11)
