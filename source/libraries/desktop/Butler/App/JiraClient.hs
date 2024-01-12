e916908ad5794d3fdb1ef80a9b86330e681f4849
Tristan Cacqueray <tdecacqu@redhat.com>
2024-01-12 14:53:06 +0000

    JiraClient: add story filters

--- source/libraries/desktop/Butler/App/JiraClient.hs
+++ source/libraries/desktop/Butler/App/JiraClient.hs
@@ -6,6 +6,7 @@ import Butler.App
 import Butler.App.PokerPlanner (pokerPlannerApp)
 import Butler.AppSettings
 
+import Data.List (nub, sort, sortOn)
 import Data.Map.Strict qualified as Map
 import Jira qualified
 import Network.HTTP.Client.TLS (newTlsManager)
@@ -24,8 +25,19 @@ jiraClientApp =
 
 data Status = Loading | Stories [Jira.JiraIssue]
 
+data Controls = Controls
+    { byName :: Bool
+    , noPoints :: Bool
+    , typeFilter :: Maybe Text
+    }
+    deriving (Generic)
+
+defaultControls :: Controls
+defaultControls = Controls False False Nothing
+
 data State = State
     { status :: Status
+    , controls :: Controls
     , client :: Maybe Jira.JiraClient
     }
     deriving (Generic)
@@ -49,13 +61,37 @@ startJiraClient ctx = do
             appSettings <- getAppSettings ctx.shared "jira-client"
             pure (into @Text <$> Map.lookup "jira-project" appSettings)
 
-    tState <- newTVarIO . State Loading =<< getClient
+    tState <- newTVarIO . State Loading defaultControls =<< getClient
 
     -- UI
+    let renderControls :: [Jira.JiraIssue] -> Controls -> HtmlT STM ()
+        renderControls stories ctrls = with div_ [class_ "flex py-1 px-2 items-center"] do
+            with div_ [class_ "grow"] mempty
+            with div_ [class_ "px-3"] "Filters stories:"
+            div_ do
+                with label_ [class_ "text-sm"] "without points: "
+                input_ $ butlerCheckbox ctx.wid "no-points" [] ctrls.noPoints Nothing
+            div_ do
+                with label_ [class_ "text-sm pl-3"] "by name: "
+                input_ $ butlerCheckbox ctx.wid "by-name" [] ctrls.byName Nothing
+            div_ do
+                with label_ [class_ "text-sm pl-3"] "by type: "
+                select_ [class_ "text-sm rounded", id_ "taskPrio", name_ "taskPrio", onChangeTrigger ctx.wid "filter"] $ do
+                    let optionAttrs name
+                            | name == "All" = case ctrls.typeFilter of
+                                Nothing -> [selected_ ""]
+                                Just _ -> []
+                            | otherwise = case ctrls.typeFilter of
+                                Just v | v == name -> [selected_ ""]
+                                _ -> []
+                    let mkOption name = option_ (optionAttrs name) (toHtml name)
+                    mkOption "All"
+                    forM_ (nub $ sort $ map (\issue -> issue.issueType) stories) mkOption
+
     let renderStories :: Jira.JiraClient -> [Jira.JiraIssue] -> HtmlT STM ()
-        renderStories client xs = do
+        renderStories client stories = do
             ul_ do
-                forM_ xs \x -> with li_ [class_ "flex items-center"] do
+                forM_ stories \x -> with li_ [class_ "flex items-center"] do
                     let jid = into @Text x.name
                     let startPlannerScript = startAppScript pokerPlannerApp ["argv" .= object ["requestor" .= ctx.wid, "story" .= jid]]
                     with button_ [class_ btnBlueClass, onclick_ startPlannerScript] "vote"
@@ -67,6 +103,11 @@ startJiraClient ctx = do
                     with span_ [class_ "ml-1"] (toHtml x.summary)
             withTrigger_ "click" ctx.wid "refresh" button_ [class_ $ "mt-4 " <> btnGreenClass] "refresh"
 
+    let applyControls ctrls =
+            (if ctrls.noPoints then filter (\issue -> isNothing issue.score) else id)
+                . (if ctrls.byName then sortOn (\issue -> issue.name) else id)
+                . (maybe id (\expectedType -> filter (\issue -> issue.issueType == expectedType)) ctrls.typeFilter)
+
     let mountUI :: HtmlT STM ()
         mountUI = with div_ [wid_ ctx.wid "w", class_ "flex flex-col"] do
             state <- lift (readTVar tState)
@@ -75,13 +116,18 @@ startJiraClient ctx = do
                     "jira-url or jira-token is missing, set them using the setting app"
                 Just client -> case state.status of
                     Loading -> "Loading..."
-                    Stories xs -> renderStories client xs
+                    Stories stories -> with div_ [class_ "flex flex-col"] do
+                        ctrls <- controls <$> lift (readTVar tState)
+                        renderControls stories ctrls
+                        renderStories client (applyControls ctrls stories)
 
     let getStories :: Jira.JiraClient -> ProcessIO (Either Text [Jira.JiraIssue])
         getStories client = runExceptT @Text do
             -- now <- liftIO getCurrentTime
-            -- pure [Jira.JiraIssue "PROJ" "test-001" "story" now Nothing "desc" Nothing,
-            --       Jira.JiraIssue "PROJ" "test-002" "story" now Nothing "desc" (Just 42)]
+            -- pure
+            --     [ Jira.JiraIssue "PROJ" "test-001" "story" now Nothing "desc" Nothing
+            --     , Jira.JiraIssue "PROJ" "test-002" "epic" now Nothing "desc" (Just 42)
+            --     ]
 
             project <-
                 lift getProject >>= \case
@@ -151,6 +197,19 @@ startJiraClient ctx = do
                                 closeApp ctx.shared ev.client wid
                                 setScore story v
                             res -> logError "Unknown result" ["ev" .= ev, "res" .= res]
+                    "no-points" -> do
+                        atomically $ modifyTVar' tState (#controls . #noPoints %~ not)
+                        sendsHtml ctx.shared.clients mountUI
+                    "by-name" -> do
+                        atomically $ modifyTVar' tState (#controls . #byName %~ not)
+                        sendsHtml ctx.shared.clients mountUI
+                    "filter" -> do
+                        case ev.body ^? key "value" . _String of
+                            Just v -> do
+                                let f = if v == "All" then Nothing else Just v
+                                atomically $ modifyTVar' tState (#controls . #typeFilter .~ f)
+                                sendsHtml ctx.shared.clients mountUI
+                            Nothing -> logError "Unknown filter" ["ev" .= ev]
                     _ -> logError "Unknown trigger" ["ev" .= ev]
                 sendsHtml ctx.shared.clients mountUI
             ev -> logError "Unknown ev" ["ev" .= ev]
