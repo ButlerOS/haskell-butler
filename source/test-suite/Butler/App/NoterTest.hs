4ea47a1b66acbc452e27b4a4963681f837093e27
Tristan Cacqueray <tdecacqu@redhat.com>
2024-07-25 20:21:32 -0400

    Replace Noter ace implementation with quill-ot

--- source/test-suite/Butler/App/NoterTest.hs
+++ source/test-suite/Butler/App/NoterTest.hs
@@ -1,5 +1,7 @@
 module Butler.App.NoterTest where
 
+-- import Butler.Core
+-- import Butler.Core.Logger
 import Butler.App.Noter
 import Butler.Prelude
 import Butler.Service.FileService (fileService)
@@ -13,25 +15,29 @@ test_noterApp = butlerAppTestCase noterApp \appTestContext appInstance -> do
 
     -- void . superviseProcess "logger" . stdoutLogger . (.logger) =<< asks os
 
-    client `assertReceivedHtml` [Contains "txt", NotContains "save-file"]
+    client `assertReceivedHtml` [Contains "editor", NotContains "save-file"]
+    client `assertReceivedJSON` ["body" .= ("" :: Text), "rev" .= (0 :: Word)]
 
     -- Request refresh should be empty
     appInstance `sendEvent` FakeTrigger client "refresh" []
-    client `assertReceivedJSON` ["text" .= ("" :: Text)]
+    client `assertReceivedJSON` ["body" .= ("" :: Text), "rev" .= (0 :: Word)]
 
     -- First insert should produce a save button
-    appInstance `sendEvent` FakeData client ["i" .= ("Helloo" :: Text), "p" .= [0, 0 :: Word]]
-    client `assertReceivedHtml` [NotContains "txt", Contains "new-file"]
+    appInstance `sendEvent` FakeValue client (toJSON (0 :: Word, ["Helloo" :: Text]))
+    client `assertReceivedJSON` ["rev" .= (1 :: Int)]
+    client `assertReceivedHtml` [NotContains "editor", Contains "new-file"]
 
     -- Delete backward works
-    appInstance `sendEvent` FakeData client ["e" .= [0, 6 :: Word], "p" .= [0, 5 :: Word]]
+    appInstance `sendEvent` FakeValue client (toJSON (1 :: Word, [5 :: Int, -1]))
+    client `assertReceivedJSON` ["rev" .= (2 :: Int)]
     appInstance `sendEvent` FakeTrigger client "refresh" []
-    client `assertReceivedJSON` ["text" .= ("Hello" :: Text)]
+    client `assertReceivedJSON` ["body" .= ("Hello" :: Text), "rev" .= (2 :: Int)]
 
     -- Insert more text works
-    appInstance `sendEvent` FakeData client ["i" .= (" World" :: Text), "p" .= [0, 5 :: Word]]
+    appInstance `sendEvent` FakeValue client (toJSON (2 :: Word, [toJSON (5 :: Int), toJSON (" World" :: Text)]))
+    client `assertReceivedJSON` ["rev" .= (3 :: Int)]
     appInstance `sendEvent` FakeTrigger client "refresh" []
-    client `assertReceivedJSON` ["text" .= ("Hello World" :: Text)]
+    client `assertReceivedJSON` ["body" .= ("Hello World" :: Text), "rev" .= (3 :: Int)]
 
 runTest :: IO ()
 runTest = defaultMain test_noterApp
