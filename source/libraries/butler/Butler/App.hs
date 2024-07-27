3a48db121b9bfe2ea6cb7c602c1aaa5a7c3dcf6f
Tristan Cacqueray <tdecacqu@redhat.com>
2024-07-27 06:58:25 -0400

    App: change sync event message from Value to Dynamic
    This change enables app to send any type with SyncEvent.

--- source/libraries/butler/Butler/App.hs
+++ source/libraries/butler/Butler/App.hs
@@ -67,16 +67,16 @@ newtype SyncName = SyncName Text
 
 data SyncEvent = SyncEvent
     { name :: SyncName
-    , body :: Value
+    , message :: Dynamic
     , reply :: TMVar Dynamic
     }
 
 instance ToJSON SyncEvent where toJSON se = object ["name" .= se.name]
 
-appCall :: Typeable a => AppInstance -> SyncName -> Value -> ProcessIO (Maybe a)
-appCall appInstance name body = do
+appCall :: Typeable a => Typeable b => AppInstance -> SyncName -> a -> ProcessIO (Maybe b)
+appCall appInstance name message = do
     mvReply <- newEmptyTMVarIO
-    writePipe appInstance.pipe (AppSync (SyncEvent name body mvReply))
+    writePipe appInstance.pipe (AppSync (SyncEvent name (toDyn message) mvReply))
     res <- atomically =<< waitTransaction 1000 (takeTMVar mvReply)
     pure $ case res of
         WaitTimeout -> Nothing
